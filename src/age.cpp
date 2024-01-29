271333c54e5fc7f23d04364b1de97675f6a0b0d3
d-cheholia <d.cheholia@partner.samsung.com>
2024-01-29 05:11:58 -0800

    Update age.cpp

--- src/age.cpp
+++ src/age.cpp
@@ -5,7 +5,7 @@ int   main( )
     std::size_t age{ };
 
     std::cout <<  "Enter your age: ";
-       std::cin >> age;
+       std::cin >>    age;
 
     std::cout <<  "You have lived for " << age * 12 << " months.\n";
     return 0;
