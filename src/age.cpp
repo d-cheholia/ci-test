8f352330b0df27bdd7bcbaf3f524a9a3b663ce98
Dmytro Cheholia <d.cheholia@partner.samsung.com>
2024-01-29 11:04:28 +0000

    add age file

---
+++ src/age.cpp
@@ -0,0 +1,12 @@
+#include <iosteam>
+
+int   main( )
+{
+    std::size_t age{ };
+
+    std::cout <<  "Enter your age: ";
+       std::cin >> age;
+
+    std::cout << "You have lived for " << age * 12 << " months.\n";
+    return 0;
+}
