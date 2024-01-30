44dd5a629ddd8e91faa84091c0eb02ce72f54b7d
Dmytro Cheholia <d.cheholia@partner.samsung.com>
2024-01-30 18:22:14 +0000

    update files

--- .universum.py
+++ .universum.py
@@ -4,7 +4,7 @@ from universum.configuration_support import Configuration, Step
 
 configs = Configuration([Step(name='Print Hello', command=['echo', 'hello'])])
 
-configs = Configuration([Step(name="clang-format", code_report=True, directory="./src", command=[
+configs = Configuration([Step(name="clang-format", code_report=True, command=[
     "python3", "-m", "universum.analyzers.clang_format", "--executable", "clang-format", "--files", "*.cpp",
     "--result-file", "${CODE_REPORT_FILE}"
 ],artifacts='code_report_results/clang-format.json')])
